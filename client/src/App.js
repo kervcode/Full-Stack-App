6aff7f638c773fe14862964919f670c14fa42c7a
Kervintz Noel <kervintz84@hotmail.com>
2020-11-10 17:46:12 -0500

    Comment app codes in app.js so data can load in Courses.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -14,32 +14,29 @@ import CourseDetail from "./components/CourseDetail"
 import './App.css';
 // import { Provider } from './components/Context/Index';
 
-
-
-function App() {
-  // const [data, setData] = useState([]) 
-  return (
-    <Router>
+class App extends Component {
+  render() { 
+    return ( 
+      <Router>
       {/* <Provider> */}
           <div>
             <Header />
             <Switch>
-              <Route exact path="/">
-                <Redirect to="/courses/api" component={()=> <Courses data={'data'} />} />
-                
-                <Redirect 
-                   to={{
-                    pathname: "/test/new",
-                    state: { property_id: property_id }
-          }}
-                />
-              </Route>
-            
+            <Route exact path="/" component={()=> <Courses data={'data'}/>}/> 
+            {/*
+              // <Redirect from='/' to='/api/courses' />
+              // <Route exact path="/api/courses" render={()=> <Courses />} />
+            */}
             </Switch>
         </div>
       {/* </Provider> */}
   </Router>
-  );
+     );
+  }
 }
+ 
+
+
+
 
 export default App;
